560700ede82779dc6c70e8bc6877ab64c6533ab7
Prajwal2112 <thombreprajwal00000@gmail.com>
2025-08-29 13:51:29 +0530

    commit

---
+++ 03-DRF-Prattern.py
@@ -0,0 +1,6 @@
+def reverse_star_triangle(rows):
+    for i in range(rows, 0, -1):
+        print(" * " * i)
+
+reverse_star_triangle(5)
+ 
---
+++ 04-SLF-pattern.py
@@ -0,0 +1,7 @@
+def slf(rows):
+        for i in range( 1, rows + 1):
+                space = " " * (rows - 1)
+                stars = " * " * i
+                print(space , stars)
+slf(5)
+ 
